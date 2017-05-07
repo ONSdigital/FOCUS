542a1913efdc12016a92d6341358bc3f46de7350
pabarber <pabarber@hotmail.co.uk>
2017-05-07 14:32:22 +0100

    start of changes to move generation of data vis teams type data to post processing and mods to deal with multiple reps.

--- FOS_main.py
+++ FOS_main.py
@@ -166,7 +166,7 @@ def produce_default_output(geog='LA'):
 if __name__ == '__main__':
 
     create_new_config = False
-    produce_default = False
+    produce_default = True
     multiple_processors = True
     freeze_support()
 
@@ -180,7 +180,7 @@ if __name__ == '__main__':
     # read in input configuration file using a default if nothing is selected
     input_path = input('Enter input file path or press enter to use defaults: ')
     if len(input_path) < 1:
-        file_name = 'inputs/CCA_small.JSON'
+        file_name = 'inputs/CCA_smaller.JSON'
 
         input_path = os.path.join(os.getcwd(), file_name)
 
--- post_process.py
+++ post_process.py
@@ -102,10 +102,32 @@ def cumulative_sum(df, start, end, step, geog, resp_type='all'):
     # add a new column to passed data frame containing the categories the entry belongs too
     df['categories'] = pd.cut(df['time'], bin_values, labels=group_names)
     # group by each combination of district and category (and rep?) and count the number of each category
-    cat_sum = df.groupby([geog, 'categories'])['categories'].size()
+    cat_sum = df.groupby([geog, 'categories', 'rep'])['categories'].size()
     # calculate the cum sum of the totals
+    print(cat_sum)
+    cat_sum = cat_sum.groupby(level=[2]).cumsum()
+    print(cat_sum)
+    cat_sum = pd.DataFrame(cat_sum).reset_index()
+    print(cat_sum)
+
+    cat_sum = cat_sum.groupby([geog, 'categories', 'rep']).sum().groupby(level=[0]).cumsum()
+    print(cat_sum)
+    counts_df = pd.DataFrame(index=group_names)
+
+    for i in range(1, 3):
+        temp_df = cat_sum[(cat_sum.rep == i)].copy()
+        temp_df.rename(columns={0: 'count'}, inplace=True)
+        temp_df['cum_sum'] = temp_df['count'].cumsum()
+        counts_df = pd.concat([counts_df, pd.DataFrame(temp_df['cum_sum'], index=temp_df['categories'])], axis=1)
+        # last col is what we want....copy to ...
+
+        # do operation on this??
+
+
     cat_sum = cat_sum.groupby(level=[0]).cumsum().reset_index(name='cum_sum')
 
+    # convert to df and split for each rep
+    print(cat_sum)
     # average by rep...
 
     # pivot it so the categories become the columns
