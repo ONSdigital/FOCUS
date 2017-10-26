85f1702ce1e28db1022732bcd3da544afe301c3e
pabarber <pabarber@hotmail.co.uk>
2017-10-26 10:06:00 +0000

    addition of code to calculate effectivness of visits.

--- household.py
+++ household.py
@@ -369,7 +369,6 @@ class Household(object):
             except:
                 pass
             self.resp_level = response_data["response"]
-
             if self.rep.total_ad_instances > 0:
                 self.help_level = response_data["help"]
             else:
